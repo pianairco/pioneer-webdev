deb5e1b046b2a3cc29db1fd2887cc34f555e11f8
rahmati <rahmati@jibit.ir>
2023-12-02 16:44:04 +0330

    improve SequenceProvider

--- common-api/src/main/java/ir/piana/dev/common/data/SequenceProvider.java
+++ common-api/src/main/java/ir/piana/dev/common/data/SequenceProvider.java
@@ -6,14 +6,28 @@ public abstract class SequenceProvider {
     private AtomicLong nextProvider;
     private AtomicLong afterFetch;
 
-    public long next() {
+    private static final AtomicLong lock = new AtomicLong(0);
+
+    public final long next() {
+        if (afterFetch != null && (afterFetch.longValue() + fetchSize() - 1) > nextProvider.longValue()) {
+            long l = nextProvider.incrementAndGet();
+            if ((afterFetch.longValue() + fetchSize() - 1) > l)
+                return l;
+            else
+                return maybeFetch();
+        } else {
+            return maybeFetch();
+        }
+    }
+
+    private synchronized long maybeFetch() {
         if (afterFetch != null && (afterFetch.longValue() + fetchSize() - 1) > nextProvider.longValue()) {
             return nextProvider.incrementAndGet();
         } else {
             afterFetch = new AtomicLong(fetch());
-            nextProvider = new AtomicLong(afterFetch.intValue());
-            return nextProvider.longValue();
         }
+        nextProvider = new AtomicLong(afterFetch.intValue());
+        return nextProvider.longValue();
     }
 
     protected abstract int fetchSize();
